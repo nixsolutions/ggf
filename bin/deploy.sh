eaef13e8504fe1c9d1f2bae40be2187374037aaf
“Maria <mironetsmidori@gmail.com>
2016-10-26 11:29:18 +0300

    Deploy script

---
+++ bin/deploy.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+# -w - workspace directory on build server
+# -u - deploy user on remote server
+# -s - server host to deploy to
+# -e - app environment
+# -c - current commit
+
+while getopts w:u:s:e:c: flag; do
+    case $flag in
+        w)
+            workspace=$OPTARG;
+            ;;
+        u)
+            user=$OPTARG;
+            ;;
+        s)
+            host="$OPTARG";
+            ;;
+        e)
+            APP_ENV="$OPTARG";
+            ;;
+        c)
+            currentCommit=$OPTARG;
+            ;;
+        ?)
+            exit;
+            ;;
+    esac
+done
+
+deployDir="/home/dev/src/ggf_${APP_ENV}"
+
+echo "DEPLOY DIR ======>  $deployDir   "
+echo "HOST       ======>  $host       "
+
+echo -e "\tSyncing data in $deployDir with git tag $currentCommit"
+
+mkdir -p $deployDir
+
+echo  " DEFAULT DEPLOY "
+rsync -aP --no-o --no-g --delete --progress $workspace/ $deployDir \
+--exclude /.buildpacks \
+--exclude /.gitattributes \
+--exclude /.gitignore \
+--exclude /.travis.yml \
+--exclude /after.sh \
+--exclude /Procfile \
+--exclude /readme.md \
+--exclude /Homestead.yaml \
+--exclude /server.php \
+--exclude /Vagrantfile \
+--exclude /vendor/ \
+--exclude /.vagrant/ \
+--exclude /node_modules/ \
+--exclude /resources/frontend/node_modules/ \
+
+echo -e "\tRunning install script/smoke tests"
+cd ${deployDir} ; \
+chmod -v +x $deployDir/bin/install.sh -h ${host} ; \
+$deployDir/bin/install.sh -e ${APP_ENV} ; "
