3f221a94cc2d3a9c62047b90f354268afbc2ba08
kvasenko <kvasenko@ukr.net>
2016-11-11 15:11:21 +0200

    response 401 if user unauthorized

--- app/Http/Requests/CreateTeam.php
+++ app/Http/Requests/CreateTeam.php
@@ -3,6 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
 
 class CreateTeam extends FormRequest
 {
@@ -13,7 +14,7 @@ class CreateTeam extends FormRequest
      */
     public function authorize()
     {
-        return true;
+        return Auth::check();
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Http\Middleware\Authenticate;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,27 +17,27 @@ use Illuminate\Http\Request;
 Route::group(['prefix' => 'v1'], function() {
 
     Route::get('/leagues', 'API\LeagueController@catalogue');
-    Route::post('/leagues', 'API\LeagueController@store');
+    Route::post('/leagues', 'API\LeagueController@store')->middleware(Authenticate::class);
     Route::get('/leagueTeams', 'API\LeagueController@teams');
 
     Route::get('/tournaments', 'API\TournamentController@catalogue');
-    Route::post('/tournaments', 'API\TournamentController@store');
+    Route::post('/tournaments', 'API\TournamentController@store')->middleware(Authenticate::class);
     Route::get('/tournaments/{tournamentId}', 'API\TournamentController@find');
-    Route::put('/tournaments/{tournamentId}', 'API\TournamentController@update');
+    Route::put('/tournaments/{tournamentId}', 'API\TournamentController@update')->middleware(Authenticate::class);
 
     Route::get('/teams', 'API\TournamentTeamController@catalogue');
-    Route::post('/teams', 'API\TournamentTeamController@add');
+    Route::post('/teams', 'API\TournamentTeamController@add')->middleware(Authenticate::class);
     Route::get('/teams/search', 'API\TeamController@search');
 
     Route::get('/teams/all', 'API\TeamController@catalogue');
-    Route::post('/leagueTeams', 'API\TeamController@store');
-    Route::delete('/leagueTeams/{teamId}', 'API\TeamController@delete');
+    Route::post('/leagueTeams', 'API\TeamController@store')->middleware(Authenticate::class);
+    Route::delete('/leagueTeams/{teamId}', 'API\TeamController@delete')->middleware(Authenticate::class);
     Route::get('/teams/{teamId}', 'API\TeamController@find');
-    Route::delete('/teams/{teamId}', 'API\TeamController@remove');
+    Route::delete('/teams/{teamId}', 'API\TeamController@remove')->middleware(Authenticate::class);
 
     Route::get('/teamMembers', 'API\TeamMemberController@catalogue');
-    Route::post('/teamMembers', 'API\TeamMemberController@assign');
-    Route::delete('/teamMembers/{teamMemberId}', 'API\TeamMemberController@remove');
+    Route::post('/teamMembers', 'API\TeamMemberController@assign')->middleware(Authenticate::class);
+    Route::delete('/teamMembers/{teamMemberId}', 'API\TeamMemberController@remove')->middleware(Authenticate::class);
     Route::get('/teamMembers/search', 'API\TeamMemberController@search');
 
     Route::get('/tablescores', 'API\TournamentController@tablescores');
